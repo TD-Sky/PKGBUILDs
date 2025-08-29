48cee6c0113e74a6dab367cd0bbeb0386eaf4cde
Kimiblock Moe <kimiblock@icloud.com>
2025-08-30 02:19:10 +0800

    Initial upload: feishu-portable 1:7.46.12-1

--- feishu-portable/feishu.sh
+++ feishu-portable/feishu.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/bash
 
-export launchTarget="/opt/bytedance/feishu/bytedance-feishu"
-if [ ${XDG_SESSION_TYPE} = "wayland" ]; then
-    export launchTarget="${launchTarget} --ozone-platform-hint=auto --enable-features=UseOzonePlatform --enable-wayland-ime --gtk-version=4"
-    export waylandOnly=${waylandOnly:-"true"}
-fi
+export _portableConfig="com.bytedance.feishu"
 
-export _portableConfig="/usr/lib/portable/info/com.bytedance.feishu/config"
-
-portable $@
+# Double dash is important!
+exec portable -- $@
--- feishu-portable/install-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -xve
-
-ln -sfr /usr/lib/feishu-portable/feishu.sh /usr/bin/feishu
-ln -sfr /usr/lib/feishu-portable/feishu.sh /usr/bin/bytedance-feishu-stable
-ln -sfr /dev/null /usr/share/applications/feishu.desktop
-sed -i 's#command="/opt/bytedance/feishu/feishu"#command="/usr/bin/feishu"#' /usr/share/menu/feishu.menu
